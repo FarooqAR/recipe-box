a500c078ad881669ae081bb29fe21b597e2a74ae
FarooqAR <farooqbckk@gmail.com>
2017-05-23 15:14:00 +0500

    add gh-pages

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import App from './App/App';
-import RecipeList from './RecipeList/RecipeList';
-import RecipeListItem from './RecipeListItem/RecipeListItem';
-import CustomDialog from './CustomDialog/CustomDialog';
-
-export {App, RecipeList, RecipeListItem, CustomDialog};
--- src/helper.js
+++
@@ -1,8 +0,0 @@
-export function guidGenerator() {
-    const s =  () => {
-        return (((1 + Math.random()) * 0x10000) | 0)
-            .toString(16)
-            .substring(1);
-    };
-    return (s() + s() + "-" + s() + "-" + s() + "-" + s() + "-" + s() + s() + s());
-}
